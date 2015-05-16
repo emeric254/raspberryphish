e7728f4ce925873f238ea43f004f27b72126250f
emeric254 <emeric254@live.fr>
2015-05-16 22:55:12 +0200

    modifié :         install/conf-raspberryphish-server.sh

--- install/conf-raspberryphish-server.sh
+++ install/conf-raspberryphish-server.sh
@@ -4,7 +4,7 @@
 echo  '#!/bin/sh'  >  "$SERVERPATH/launcher-raspberryphish-server.sh"
 echo  '# launcher for raspberryphish server'  >>  "$SERVERPATH/launcher-raspberryphish-server.sh"
 echo  "cd $SERVERPATH"  >>  "$SERVERPATH/launcher-raspberryphish-server.sh"
-echo  "python3 main.py > ./logs/log-$(date +%Y-%m-%d_%H-%M)"  >>  "$SERVERPATH/launcher-raspberryphish-server.sh"
+echo  "python3 main.py 1>> ./logs/log-$(date +%Y-%m-%d_%H-%M) 2>> ./logs/log-error-$(date +%Y-%m-%d_%H-%M)"  >>  "$SERVERPATH/launcher-raspberryphish-server.sh"
 
 # make it launchable
 chmod 755 "$SERVERPATH/launcher-raspberryphish-server.sh"
@@ -17,7 +17,7 @@ cp -r  ./server/*  "$SERVERPATH/"
 mkdir  "$SERVERPATH/logs"
 
 # configure cron-file
-echo  "@reboot sh $SERVERPATH/launcher-raspberryphish-server.sh > $SERVERPATH/logs/cronlog 2>&1"  >  ./install/cron-file
+echo  "@reboot sh $SERVERPATH/launcher-raspberryphish-server.sh 1>> $SERVERPATH/logs/cronlog 2>> $SERVERPATH/logs/cronlog"  >  ./install/cron-file
 
 # load it
 crontab ./install/cron-file
