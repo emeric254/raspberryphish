46fc945992fc87310028800fbd07039c461a4e0e
emeric254 <emeric254@live.fr>
2015-05-16 22:19:29 +0200

    debug conf server

--- install/conf-raspberryphish-server.sh
+++ install/conf-raspberryphish-server.sh
@@ -1,13 +1,14 @@
 
-# cp launcher script
-cp  ./install/launcher-raspberryphish-server.sh  "$SERVERPATH/"
-chmod 755 "$SERVERPATH/launcher-raspberryphish-server.sh"
 
-# configure it
+# configure launcher script
 echo  '#!/bin/sh'  >  "$SERVERPATH/launcher-raspberryphish-server.sh"
-echo  '# launcher for raspberryphish server'  >  "$SERVERPATH/launcher-raspberryphish-server.sh"
+echo  '# launcher for raspberryphish server'  >>  "$SERVERPATH/launcher-raspberryphish-server.sh"
 echo  "cd $SERVERPATH"  >>  "$SERVERPATH/launcher-raspberryphish-server.sh"
 echo  "python3 main.py >> ./logs/log-$(date +%Y%m%d)"  >>  "$SERVERPATH/launcher-raspberryphish-server.sh"
+
+# make it launchable
+chmod 755 "$SERVERPATH/launcher-raspberryphish-server.sh"
+
 
 # copy server in the given directory
 cp -r  ./server/*  "$SERVERPATH/"
