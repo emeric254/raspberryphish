8bf8a51e99e4fad1de762963ed67e1c73cf8c440
emeric254 <emeric254@live.fr>
2015-05-21 10:09:45 +0200

    test redirection port 80

--- install/conf-iptables.sh
+++ install/conf-iptables.sh
@@ -7,12 +7,12 @@ iptables -F
 iptables -i wlan0 -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 iptables -i wlan0 -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -i wlan0 -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -i wlan0 -A INPUT -p tcp --dport $SSHPORT -j ACCEPT
 iptables -i wlan0 -A INPUT -p udp --dport 53 -j ACCEPT
 iptables -i wlan0 -A INPUT -p udp --dport 67:68 -j ACCEPT
 
-iptables -t nat -A PREROUTING -p tcp -i wlan0 --dport 443 -j DNAT --to-destination 127.0.0.1:80
-iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+iptables -t nat -A PREROUTING -p tcp -i wlan0 --dport 443 -j DNAT --to-port 80
 
 iptables -i wlan0 -A INPUT -j DROP
 
