e52f4c7e069cc24a79e2f37c8287cf9a7333ce54
emeric254 <emeric254@live.fr>
2015-06-01 22:53:41 +0200

    debug

--- install/conf-hostapd.sh
+++ install/conf-hostapd.sh
@@ -23,18 +23,12 @@ auth_server_addr=$AUTHSERVER
 auth_server_port=$AUTHPORT
 auth_server_shared_secret=$RADIUSSECRET
 auth_algs=3
-acct_server_addr=$AUTHPORT
+acct_server_addr=$ACCTSERVER
 acct_server_port=$ACCTPORT
 acct_server_shared_secret=$RADIUSSECRET
 own_ip_addr=127.0.0.1
 EOF
-    #~
-    #~ echo "auth_server_addr=$AUTHSERVER" >> /etc/hostapd/hostapd.conf
-    #~ echo "auth_server_port=$AUTHPORT" >> /etc/hostapd/hostapd.conf
-    #~ echo "acct_server_addr=$ACCTSERVER" >> /etc/hostapd/hostapd.conf
-    #~ echo "acct_server_port=$ACCTPORT" >> /etc/hostapd/hostapd.conf
-    #~ echo "auth_server_shared_secret=$RADIUSSECRET" >> /etc/hostapd/hostapd.conf
-    #~ echo "acct_server_shared_secret=$RADIUSSECRET" >> /etc/hostapd/hostapd.conf
+
 fi
 
 sed -i '/DAEMON_CONF=/c DAEMON_CONF=/etc/hostapd/hostapd.conf' /etc/init.d/hostapd
