b2937f71302a26eb598a9d8525674f1295c571fb
emeric254 <emeric254@live.fr>
2015-06-02 23:34:45 +0200

    fix

--- install/conf-network-interfaces.sh
+++ install/conf-network-interfaces.sh
@@ -12,9 +12,9 @@ cat <<EOF >>  /etc/network/interfaces
 iface $INTERFACE inet static
 allow-hotplug $INTERFACE
 address 10.0.0.1
-network 10.0.0.0
-netmask 255.255.255.0
-broadcast 10.0.0.255
-dns-nameservers 127.0.0.1
+#network 10.0.0.0
+#netmask 255.255.255.0
+#broadcast 10.0.0.255
+#dns-nameservers 127.0.0.1
 pre-up iptables-restore < /etc/iptables.rules
 EOF
