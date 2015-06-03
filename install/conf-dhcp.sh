49ec8a4a23003fe37211b91232c1265522caa7a9
emeric254 <emeric254@live.fr>
2015-06-03 10:29:04 +0200

    maj + fix

--- install/conf-dhcp.sh
+++ install/conf-dhcp.sh
@@ -5,15 +5,9 @@ cat <<EOF > /etc/udhcpd.conf
 start       10.0.0.2
 end     10.0.0.254
 interface   $INTERFACE
-max_leases 254
-auto_time  3600
-decline_time   1800
-conflict_time  1800
-offer_time 60
-min_lease  60
+remaining yes
 opt dns 10.0.0.1
 option  subnet  255.255.255.0
 opt router  10.0.0.1
-option  domain  local
-option  lease   1000
+option  lease   1800
 EOF
--- install/conf-network-interfaces.sh
+++ install/conf-network-interfaces.sh
@@ -1,9 +1,12 @@
 
 cat <<EOF >  /etc/network/interfaces
 auto lo
-
 iface lo inet loopback
+
+auto eth0
+allow-hotplug eth0
 iface eth0 inet dhcp
+iface eth0 inet6 auto
 
 EOF
 
@@ -11,10 +14,11 @@ EOF
 cat <<EOF >>  /etc/network/interfaces
 allow-hotplug $INTERFACE
 iface $INTERFACE inet static
-address 10.0.0.1
-#network 10.0.0.0
-#netmask 255.255.255.0
-#broadcast 10.0.0.255
-#dns-nameservers 127.0.0.1
+    address 10.0.0.1
+    netmask 255.255.255.0
+    gateway 10.0.0.1
+    #network 10.0.0.0
+    #broadcast 10.0.0.255
+    #dns-nameservers 127.0.0.1
 pre-up iptables-restore < /etc/iptables.rules
 EOF
