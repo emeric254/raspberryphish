d29f12b6c86e4a61c40ff26d63311c328b62b6b0
emeric254 <emeric254@live.fr>
2015-06-03 15:18:46 +0200

    debug

--- install/conf-update-rc.sh
+++ install/conf-update-rc.sh
@@ -1,6 +1,27 @@
 
-update-rc.d hostapd defaults
-update-rc.d dnsmasq defaults
+#~ update-rc.d hostapd defaults
+#~ update-rc.d dnsmasq defaults
+
+
+# configure launcher script
+cat <<EOF > "$SERVERPATH/launcher-raspberryphish-server.sh"
+#!/bin/sh
+# launcher for raspberryphish server
+ifup $INTERFACE
+/etc/init.d/dnsmasq start
+/etc/init.d/hostapd start
+sleep 5
+cd $SERVERPATH
+python3 main.py 1>> ./logs/log-$(date +%Y-%m-%d_%H-%M) 2>> ./logs/log-error-$(date +%Y-%m-%d_%H-%M) &
+EOF
+
+# make it launchable
+chmod 755 "$SERVERPATH/launcher-raspberryphish-server.sh"
+
+# configure cron-file
+echo  "@reboot sh $SERVERPATH/launcher-raspberryphish-server.sh 1> $SERVERPATH/logs/cronlog 2> $SERVERPATH/logs/cronlog-error &"  >>  ./install/cron-file
+
+
 
 
 if [ $ACTIVATERADIUS -eq 1 ]
@@ -16,6 +37,8 @@ EOF
 chmod 755 "$SERVERPATH/launcher-radius-server.sh"
 # add this conf to cron-file
 echo  "@reboot sh $SERVERPATH/launcher-radius-server.sh 1> $RADIUSLOGPATH/cronlog 2> $RADIUSLOGPATH/cronlog-error &"  >>  ./install/cron-file
+fi
+
+
 # load it
 crontab ./install/cron-file
-fi
