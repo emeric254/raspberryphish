04679da969188b59df7c34430fe864126a11bd01
emeric254 <emeric254@live.fr>
2015-06-03 13:08:59 +0200

    test

--- install/conf-dnsmasq.sh
+++ install/conf-dnsmasq.sh
@@ -1,6 +1,5 @@
 
 cat <<EOF > /etc/dnsmasq.conf
-no-resolv
 interface=$INTERFACE
 dhcp-authoritative
 dhcp-leasefile=/tmp/dhcp.leases
@@ -8,6 +7,8 @@ dhcp-option=1,255.255.255.0
 dhcp-option=3,10.0.0.254 # default gateway
 dhcp-option=6,10.0.0.254 # dns server
 dhcp-range=10.0.0.10,10.0.0.99,255.255.255.0,12h
+
+no-resolv
 address=/#/10.0.0.254
 log-queries
 log-facility=/var/log/dnsmasq.log
