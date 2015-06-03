a8ac46a2c23252700b8ac07bd907d3fa684ccb05
emeric254 <emeric254@live.fr>
2015-06-03 12:50:46 +0200

    test

--- install/conf-dnsmasq.sh
+++ install/conf-dnsmasq.sh
@@ -3,6 +3,7 @@ cat <<EOF > /etc/dnsmasq.conf
 no-resolv
 interface=$INTERFACE
 dhcp-range=10.0.0.10,10.0.0.200,255.255.255.0,12h
+dhcp-option=3,10.0.0.1
 address=/#/10.0.0.1
 log-queries
 log-facility=/var/log/dnsmasq.log
--- install/conf-network-interfaces.sh
+++ install/conf-network-interfaces.sh
@@ -14,6 +14,7 @@ iface $INTERFACE inet static
     address 10.0.0.1
     netmask 255.255.255.0
     broadcast 10.0.0.255
+    network 10.0.0.0
 pre-up iptables-restore < /etc/iptables.rules
 
 #allow-hotplug wlan0
