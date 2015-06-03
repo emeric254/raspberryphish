2ad2df5b68982173c37d9847041e7cb6085d9c9a
emeric254 <emeric254@live.fr>
2015-06-03 10:50:25 +0200

    maj

--- install/conf-iptables.sh
+++ install/conf-iptables.sh
@@ -7,11 +7,17 @@ iptables -F
 sleep 1
 
 iptables -i wlan0 -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
 iptables -i wlan0 -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -i wlan0 -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -i wlan0 -A INPUT -p tcp --dport $SSHPORT -j ACCEPT
 iptables -i wlan0 -A INPUT -p udp --dport 53 -j ACCEPT
 iptables -i wlan0 -A INPUT -p udp --dport 67:68 -j ACCEPT
+
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+
 iptables -i wlan0 -A INPUT -j DROP
 
 
