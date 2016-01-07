10b44e0c4b49d0229ec6a707127d16b08a8df2ec
emeric254 <emeric254@live.fr>
2016-01-07 19:43:41 +0100

    admin ihm api work

--- server/rsc/admin/src/APIController.js
+++ server/rsc/admin/src/APIController.js
@@ -9,7 +9,7 @@
         {
             var self = this;
 
-	        self.loadchartlabels = ["CPU", "RAM", "Network", "Disk", "Temp"];
+	        self.loadchartlabels = ["CPU", "RAM", "Network", "Disk-IO", "Disk-Space"];
 	        self.loadchartseries = ['Current'];
 	        self.loadchartdata = [
 	            [0, 0, 0, 0, 0]
@@ -30,9 +30,12 @@
                         {
                             self.loadchartdata[0][0] = response.CPU;
                             self.loadchartdata[0][1] = response.RAM;
-                            self.loadchartdata[0][3] = response.Storage;
+                            //self.loadchartdata[0][2] = response.Network;
+                            //self.loadchartdata[0][3] = response.IO;
+                            self.loadchartdata[0][4] = response.Storage;
+                            // refresh in 1 second
+                            setTimeout(getSystemLoad, 1000);
                         });
-                setTimeout(getSystemLoad, 1000);
             }
         }
     }
