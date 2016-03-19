ee298ef3a0aabc004842210840d4b7144f08a191
emeric254 <emeric254@live.fr>
2016-03-19 13:49:15 +0100

    add conf loader and cleanup main.py

--- server/main.py
+++ server/main.py
@@ -3,13 +3,9 @@
 
 import os
 import ssl
-import time
+import configparser
 
-import tornado.httpserver
-import tornado.ioloop
-import tornado.netutil
-import tornado.process
-import tornado.web
+from tornado import httpserver, ioloop, netutil, process, web
 # from tornado import gen
 
 from server.Handlers.MainHandler import MainHandler
@@ -19,49 +15,56 @@ from server.Handlers.AdminHandler import AdminHandler
 # app's title
 __title__ = 'RaspberryPhishServer'
 
+# load configuration from 'configuration.conf' file
+config = configparser.ConfigParser()
+config.read('configuration.conf')
+if 'SERVER' not in config:
+    raise ValueError('Please verify your configuration file contains a [SERVER] section')
+if 'page_path' not in config['SERVER'] or 'http_port' not in config['SERVER'] or 'https_port' not in config['SERVER']:
+    raise ValueError('Please verify [SERVER] section of your configuration file')
 # var : directory name where the server will load in 'pages' and 'rsc'
-pagePath = 'test/'
-http_port = 8080
-https_port = 4430
+page_path = config['SERVER']['page_path']
+http_port = config['SERVER']['http_port']
+https_port = config['SERVER']['https_port']
 
 
 def main():
     # create an instance
-    application = tornado.web.Application([
-            (r'/rsc/(.*)', tornado.web.StaticFileHandler, {'path': 'rsc/'}),
+    application = web.Application([
+            (r'/rsc/(.*)', web.StaticFileHandler, {'path': 'rsc/'}),
             (r'/API/(.*)$', APIHandler),
             (r'/admin', AdminHandler),
             (r'/admin/(.*)$', AdminHandler),
-            (r'/', MainHandler, dict(page_path=pagePath)),
-            (r'/.*', MainHandler, dict(page_path=pagePath))
+            (r'/', MainHandler, dict(page_path=page_path)),
+            (r'/.*', MainHandler, dict(page_path=page_path))
         ])
     # HTTP socket
-    http_socket = tornado.netutil.bind_sockets(http_port)
+    http_socket = netutil.bind_sockets(http_port)
     # HTTPS socket
-    https_socket = tornado.netutil.bind_sockets(https_port)
+    https_socket = netutil.bind_sockets(https_port)
     # fork, except KeyboardInterrupt to properly exit
     try:
-        tornado.process.fork_processes(0)
+        process.fork_processes(0)
     except KeyboardInterrupt:
-        tornado.ioloop.IOLoop.current().stop()
+        ioloop.IOLoop.current().stop()
     # try loading ssl to purpose https
-    cert_file = 'cert/' + pagePath + 'default.cert'
-    key_file = 'cert/' + pagePath + 'default.key'
+    cert_file = 'cert/' + page_path + 'default.cert'
+    key_file = 'cert/' + page_path + 'default.key'
     if os.path.isfile(cert_file) and os.path.isfile(key_file):
         # load ssl requirements
         ssl_ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
         ssl_ctx.load_cert_chain(cert_file, key_file)
         # bind https port
         print('Start an HTTPS request handler on port : ' + str(https_port))
-        tornado.httpserver.HTTPServer(application, ssl_options=ssl_ctx).add_sockets(https_socket)
+        httpserver.HTTPServer(application, ssl_options=ssl_ctx).add_sockets(https_socket)
     # bind http port
     print('Start an HTTP request handler on port : ' + str(http_port))
-    tornado.httpserver.HTTPServer(application).add_sockets(http_socket)
+    httpserver.HTTPServer(application).add_sockets(http_socket)
     # loop forever to satisfy user's requests, except KeyboardInterrupt to properly exit
     try:
-        tornado.ioloop.IOLoop.current().start()
+        ioloop.IOLoop.current().start()
     except KeyboardInterrupt:
-        tornado.ioloop.IOLoop.current().stop()
+        ioloop.IOLoop.current().stop()
 
 
 if __name__ == '__main__':
